9ef747dfc9692bf724848aef30c29409da17c869
Mega4268 <portethan25@gmail.com>
2020-12-24 20:06:43 -0500

    added pokemon api python

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,12 @@
-print()
-print("I WANT PS5")
-print()
+import requests
 
-x = input("whats your name?")
+x = requests.get("https://pokeapi.co/api/v2/gender/3/")
 
-print()
-print("hello, " + x + "!")
-print()
+data = x.json()['pokemon_species_details']
+
+import pandas as pd
+from pandas import json_normalize as from_json
+
+df = from_json(data)
+
+print(df.head(50))
